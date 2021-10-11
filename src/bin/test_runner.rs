9288cab0e02476b52f86005dd8e3dac929773a57
Cainã Costa <me@cfcosta.com>
2021-10-11 00:35:11 -0300

    Extract run_command into crate root

--- src/bin/test_runner.rs
+++ src/bin/test_runner.rs
@@ -1,13 +1,13 @@
 use std::fs;
 use std::path::PathBuf;
 
-use anyhow::{ Result, bail };
+use anyhow::Result;
 use structopt::StructOpt;
+
 use xq::{
     parser,
-    storage::{ StorageBackend, Storage, StorageOptions },
-    types::*,
-    errors::*
+    storage::{Storage, StorageOptions},
+    run_command
 };
 
 #[derive(Clone, Debug, StructOpt)]
@@ -15,57 +15,7 @@ pub struct Options {
     #[structopt(name = "FILE")]
     file: PathBuf,
     #[structopt(flatten)]
-    storage: StorageOptions
-}
-
-enum CommandResult {
-    Empty,
-    Val(Value),
-}
-
-fn run_command(storage: &mut dyn StorageBackend, command: Command) -> Result<CommandResult> {
-    match command {
-        Command::Enqueue(key, value) => {
-            storage.enqueue(key, value)?;
-            Ok(CommandResult::Empty)
-        }
-        Command::Dequeue(key) => {
-            let value = storage.dequeue(key.clone())?;
-            Ok(CommandResult::Val(value))
-        }
-        Command::Length(key) => {
-            let value = storage.length(key.clone())?;
-            Ok(CommandResult::Val(Value::Integer(value as i64)))
-        }
-        Command::Peek(key) => {
-            let value = storage.peek(key.clone())?;
-            Ok(CommandResult::Val(value.clone()))
-        }
-        Command::Assert(cmd, val) => {
-            let cmd_desc = format!("{:?}", &cmd);
-
-            match run_command(storage, *cmd)? {
-                CommandResult::Val(result) => {
-                    if result == val {
-                        return Ok(CommandResult::Empty);
-                    }
-
-                    bail!(DataError::FailedAssertion {
-                        command: cmd_desc,
-                        expected: format!("{:?}", val),
-                        got: Some(format!("{:?}", result)),
-                    })
-                }
-                CommandResult::Empty => {
-                    bail!(DataError::FailedAssertion {
-                        command: cmd_desc,
-                        expected: format!("{:?}", val),
-                        got: None
-                    })
-                }
-            }
-        }
-    }
+    storage: StorageOptions,
 }
 
 fn main() -> Result<()> {
