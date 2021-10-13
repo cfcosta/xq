deca7586e4ae0727910b48c5097d1d7240fe800e
Cainã Costa <me@cfcosta.com>
2021-10-13 03:17:35 -0300

    Clean up the rocksdb implementation more

--- src/storage/rocksdb.rs
+++ src/storage/rocksdb.rs
@@ -1,4 +1,4 @@
-use std::sync::{Arc, RwLock};
+use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
 use rocksdb::{WriteBatch, DB};
@@ -16,21 +16,32 @@ pub struct StorageOptions {
 
 #[derive(Debug, Clone)]
 pub struct RocksDBStorage {
-    db: Arc<RwLock<DB>>,
+    db: Arc<DB>,
 }
 
 impl RocksDBStorage {
     #[tracing::instrument]
     pub fn init(path: &str) -> Result<Self> {
         Ok(Self {
-            db: Arc::new(RwLock::new(
+            db: Arc::new(
                 DB::open_default(path).map_err(|_| StorageError::FailedInitialize)?,
-            )),
+            ),
         })
     }
 
     fn bound_keys(&self, id: &Identifier) -> (String, String) {
         (format!("{}:begin", &id.0), format!("{}:end", &id.0))
+    }
+
+    fn bounds_for(&self, id: &Identifier) -> Result<(u64, u64)> {
+        let db = self.db.clone();
+
+        let (begin_key, end_key) = self.bound_keys(id);
+
+        let begin = db.get(&begin_key)?.ok_or(anyhow!(DataError::EmptyQueue(id.to_string())))?;
+        let end = db.get(&end_key)?.ok_or(anyhow!(DataError::EmptyQueue(id.to_string())))?;
+
+        Ok((serde_json::from_slice(&begin)?, serde_json::from_slice(&end)?))
     }
 }
 
@@ -38,19 +49,17 @@ impl RocksDBStorage {
 impl StorageBackend for RocksDBStorage {
     #[tracing::instrument]
     async fn enqueue(&self, id: Identifier, value: Value) -> Result<()> {
-        let db = self.db.write().map_err(|_| StorageError::FailedLock)?;
+        let db = self.db.clone();
 
         let (begin_key, end_key) = self.bound_keys(&id);
         let mut batch = WriteBatch::default();
 
-        match db.get(&end_key)? {
-            Some(end) => {
-                let next: u64 = serde_json::from_slice::<u64>(&end)? + 1;
-
-                batch.put(&end_key, serde_json::to_vec(&next)?);
-                batch.put(&format!("{}:{}", &id.0, next), serde_json::to_vec(&value)?);
+        match self.bounds_for(&id) {
+            Ok((_, end)) => {
+                batch.put(&end_key, serde_json::to_vec(&(end + 1))?);
+                batch.put(&format!("{}:{}", &id.0, &(end + 1)), serde_json::to_vec(&value)?);
             }
-            None => {
+            _ => {
                 batch.put(&begin_key, serde_json::to_vec(&0)?);
                 batch.put(&end_key, serde_json::to_vec(&0)?);
                 batch.put(&format!("{}:{}", &id.0, 0), serde_json::to_vec(&value)?);
@@ -64,56 +73,34 @@ impl StorageBackend for RocksDBStorage {
 
     #[tracing::instrument]
     async fn dequeue(&self, id: Identifier) -> Result<Value> {
-        let db = self.db.write().map_err(|_| StorageError::FailedLock)?;
+        let db = self.db.clone();
 
         let (begin_key, _) = self.bound_keys(&id);
-
-        let begin_data = db
-            .get(&begin_key)?
-            .ok_or(anyhow!(DataError::EmptyQueue(id.0.clone())))?;
-        let begin = serde_json::from_slice::<u64>(&begin_data)?;
-        let next = begin + 1;
+        let (begin, _) = self.bounds_for(&id)?;
 
         let data = db
             .get(&format!("{}:{}", &id.0, begin))?
             .ok_or(anyhow!(DataError::EmptyQueue(id.0.clone())))?;
 
-        db.put(&begin_key, serde_json::to_vec(&next)?)?;
+        db.put(&begin_key, serde_json::to_vec(&(begin + 1))?)?;
 
         Ok(serde_json::from_slice::<Value>(&data)?)
+
     }
 
     #[tracing::instrument]
     async fn length(&self, id: Identifier) -> Result<usize> {
-        let db = self.db.read().map_err(|_| StorageError::FailedLock)?;
-
-        let (begin_key, _) = self.bound_keys(&id);
-        let end_key = format!("{}:end", &id.0);
-
-        match db.get(&begin_key)? {
-            Some(begin_data) => {
-                let begin = serde_json::from_slice::<u64>(&begin_data)?;
-                let end_data = db
-                    .get(&end_key)?
-                    .ok_or(anyhow!(DataError::EmptyQueue(id.0)))?;
-                let end = serde_json::from_slice::<u64>(&end_data)?;
-
-                Ok((end - begin + 1) as usize)
-            }
-            None => Ok(0),
+        match self.bounds_for(&id) {
+            Ok((begin, end)) => Ok((end - begin + 1) as usize),
+            _ => Ok(0)
         }
     }
 
     #[tracing::instrument]
     async fn peek(&self, id: Identifier) -> Result<Value> {
-        let db = self.db.read().map_err(|_| StorageError::FailedLock)?;
+        let db = self.db.clone();
 
-        let (begin_key, _) = self.bound_keys(&id);
-
-        let begin_data = db
-            .get(&begin_key)?
-            .ok_or(anyhow!(DataError::EmptyQueue(id.clone().0)))?;
-        let begin = serde_json::from_slice::<u64>(&begin_data)?;
+        let (begin, _) = self.bounds_for(&id)?;
         let data = db
             .get(&format!("{}:{}", &id.0, begin))?
             .ok_or(anyhow!(DataError::EmptyQueue(id.0)))?;
