7179a4f36987e3e96b165cb270f967e41060d121
Cainã Costa <me@cfcosta.com>
2021-11-02 17:56:16 -0300

    Start new storage before each bench

--- benches/commands.rs
+++ benches/commands.rs
@@ -1,64 +1,74 @@
+use std::fmt::Debug;
+
 use anyhow::Result;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use mktemp::Temp;
 
-use xq::{parser, run_command, storage::Storage, types::*};
+use xq::{
+    parser, run_command,
+    storage::{Storage, StorageBackend},
+    types::*,
+};
 
-fn criterion_benchmark(c: &mut Criterion) -> Result<()> {
+fn setup_storage() -> Result<impl StorageBackend + Debug + Sync> {
     let path = Temp::new_dir()?.to_path_buf().display().to_string();
 
     #[cfg(feature = "memory-storage")]
-    let storage = Storage::new();
-    #[cfg(feature = "rocksdb-storage")]
-    let storage = Storage::init(&path)?;
+    return Ok(Storage::new());
 
+    #[cfg(feature = "rocksdb-storage")]
+    return Ok(Storage::init(&path)?);
+}
+
+fn criterion_benchmark(c: &mut Criterion) -> Result<()> {
     c.bench_function("parsing", |b| {
         b.iter(|| {
             assert_eq!(
                 parser::parse("enqueue a 1").unwrap(),
-                vec![Command::enqueue("a", 1)]
+                black_box(vec![Command::enqueue("a", 1)])
             );
         })
     });
 
+    let storage = setup_storage().unwrap();
     c.bench_function("enqueue", |b| {
         b.iter(|| run_command(&storage, black_box(Command::enqueue("a", 1))).unwrap())
     });
 
+    let storage = setup_storage().unwrap();
+    run_command(&storage, black_box(Command::enqueue("multipeeks", 1))).unwrap();
     c.bench_function("multiple peeks", |b| {
         b.iter(|| {
-            run_command(&storage, black_box(Command::enqueue("multipeeks", 1))).unwrap();
-
-            for _ in 1..100 {
-                run_command(&storage, black_box(Command::peek("multipeeks"))).unwrap();
-            }
+            run_command(&storage, black_box(Command::peek("multipeeks"))).unwrap();
         });
     });
 
+    let storage = setup_storage().unwrap();
     c.bench_function("enqueue + dequeue", |b| {
+        run_command(&storage, black_box(Command::enqueue("b", 1))).unwrap();
         b.iter(|| {
-            run_command(&storage, black_box(Command::enqueue("b", 1))).unwrap();
             run_command(&storage, black_box(Command::dequeue("b"))).unwrap();
         })
     });
+
+    let storage = setup_storage().unwrap();
+    for _ in 1..100 {
+        run_command(&storage, black_box(Command::enqueue("b", 1))).unwrap();
+    }
 
     c.bench_function("enqueue * 100 + dequeue", |b| {
         b.iter(|| {
-            for _ in 1..100 {
-                run_command(&storage, black_box(Command::enqueue("b", 1))).unwrap();
-            }
-
             run_command(&storage, black_box(Command::dequeue("b"))).unwrap();
         })
     });
 
+    let storage = setup_storage().unwrap();
+    for _ in 1..1000 {
+        run_command(&storage, black_box(Command::enqueue("c", 1))).unwrap();
+    }
     c.bench_function("enqueue * 1000 + dequeue", |b| {
         b.iter(|| {
-            for _ in 1..1000 {
-                run_command(&storage, black_box(Command::enqueue("b", 1))).unwrap();
-            }
-
-            run_command(&storage, black_box(Command::dequeue("b"))).unwrap();
+            run_command(&storage, black_box(Command::dequeue("c"))).unwrap();
         })
     });
 
